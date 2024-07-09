586c8bff1889abd9dcfaca7e8ef0e1d051aed5e8
yayat_99 <rahmat999ht@gmail.com>
2024-07-09 16:10:54 +0800

    view reading done

--- lib/app/modules/reading_book/controllers/reading_book_controller.dart
+++ lib/app/modules/reading_book/controllers/reading_book_controller.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:sansgen/model/books.dart';
 
 class ReadingBookController extends GetxController {
   var scaffoldKey = GlobalKey<ScaffoldState>();
+  var book = Get.arguments['book'] as BookModel;
+  var chapter = Get.arguments['chapter'] as int;
+  final Rx<int> currentChapter = 0.obs;
 
   void openDrawer() {
     scaffoldKey.currentState?.openDrawer();
@@ -10,5 +14,33 @@ class ReadingBookController extends GetxController {
 
   void closeDrawer() {
     scaffoldKey.currentState?.openEndDrawer();
+  }
+
+  void setCurrentChapter(int chapter) {
+    currentChapter.value = chapter;
+    Get.back();
+  }
+
+  void previousChapter() {
+    if (chapter == 1) {
+      Get.snackbar('info', 'Chapter 1 is the first chapter');
+    } else {
+      chapter--;
+    }
+  }
+
+  void nextChapter() {
+    if (chapter == book.listChapter.length) {
+      Get.snackbar(
+          'info', 'Chapter ${book.listChapter.length} is the last chapter');
+    } else {
+      chapter++;
+    }
+  }
+
+  @override
+  void onInit() {
+    currentChapter.value = chapter;
+    super.onInit();
   }
 }
