4b02cdec5ba2ce965cbe8055677bae28eccd1e0f
yayat_99 <rahmat999ht@gmail.com>
2024-08-01 11:33:14 +0800

    update hide bar

--- lib/app/modules/reading_book/views/reading_book_view.dart
+++ lib/app/modules/reading_book/views/reading_book_view.dart
@@ -1,9 +1,5 @@
 import 'dart:developer';
 
-import 'dart:developer';
-
-import 'dart:developer';
-
 import 'package:circular_countdown_timer/circular_countdown_timer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
