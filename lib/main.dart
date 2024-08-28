ff6a30138b0be69aad8756c6fbda1beaec09c4cf
yayat_99 <rahmat999ht@gmail.com>
2024-08-29 00:26:47 +0800

    splach and icon app

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,12 @@ import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
+import 'package:sansgen/keys/assets_images.dart';
 import 'package:sansgen/services/prefs.dart';
 import 'package:sansgen/theme/app_theme.dart';
+import 'package:sansgen/widgets/image_book.dart';
 
 import 'app/routes/app_pages.dart';
 
@@ -24,7 +27,22 @@ Future main() async {
   String initialRoutes = isToken == '' ? Routes.LOGIN : onBoarding;
   // String initialRoutes = Routes.DETAIL;
   runApp(MyApp(initialRoutes));
+  // runApp(FutureBuilder(
+  //   future: Future.delayed(const Duration(seconds: 3)),
+  //   builder: (context, s) {
+  //     if (s.connectionState == ConnectionState.waiting) {
+  //       return const MaterialApp(
+  //         title: "Pulse Detector",
+  //         debugShowCheckedModeBanner: false,
+  //         home: SplashScreen(),
+  //       );
+  //     } else {
+  //       return MyApp(initialRoutes);
+  //     }
+  //   },
+  // ));
 }
+
 
 class MyApp extends StatelessWidget {
   final String initialRoutes;
@@ -40,6 +58,29 @@ class MyApp extends StatelessWidget {
       getPages: AppPages.routes,
       initialRoute: initialRoutes,
       builder: EasyLoading.init(),
+    );
+  }
+}
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset(
+              KeysAssetsImages.splashScreen,
+              height: Get.height,
+              width: Get.width,
+              fit: BoxFit.cover,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
